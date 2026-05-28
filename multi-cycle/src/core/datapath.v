3c3d8e83adc7c904e6460a3a3bc58082d21c2c63
Kenshin <the.kenshina.1421@gmail.com>
2026-05-29 03:42:29 +0530

    Muti Cycle Microarchitecture implementation of RV32I , Project Setup

---
+++ multi-cycle/src/core/datapath.v
@@ -0,0 +1,17 @@
+module dataPath(
+    input clk,
+    input pcUpdate,
+    input memWrite,
+    input IRWrite,
+    input regWrite,
+    input ALUSrcA,
+    input ALUSrcB,
+    input ALUCtrl,
+    input resSrc,
+    output [31:0] instOut
+);
+
+    //Architectural State Elements
+    //Non Architectural State Elements
+    //Combinational Elements
+endmodule
